b14943f8f52725a1ff754d419adadfa2a39a7d38
Amitosh Kumar <amitoshkumar10@gmail.com>
2021-10-27 19:32:55 +0530

    added validation method

--- app/app.py
+++ app/app.py
@@ -4,6 +4,8 @@ from flask import request
 from pySdk.idToken import idToken
 from pySdk.accessToken import accessToken
 from flask_cors import CORS
+from datetime import timezone
+import datetime
 app = Flask(__name__)
 CORS(app)
 
@@ -13,7 +15,37 @@ def App():
     
     token = request.data
     objs=json.loads(token)
-    x=objs['accessToken']
-    print(accessToken(x).getUserId());
-    print(accessToken(x).decodePayload())
-    return accessToken(x).decodePayload();
+    token=objs['accessToken']
+    #  signature verification
+
+    if not accessToken(token).decodePayload():
+        print("signature is invalid")
+        return {};
+    if not validateAddtionalAttributes(token):
+        print("invalid additional attributes")
+        return {};
+    print(accessToken(token).getUserId());
+    print(accessToken(token).decodePayload())
+    return accessToken(token).decodePayload();
+    
+
+   
+
+def validateAddtionalAttributes(token):
+        dt = datetime.datetime.now(timezone.utc)
+        utc_time = dt.replace(tzinfo=timezone.utc)
+        utc_timestamp = utc_time.timestamp()
+        
+        if(accessToken(token).getIss()!="https://www.secuuth.io"):
+            return False
+        if(accessToken(token).getAud()!="***************************"):
+            return False;
+        if(utc_timestamp-accessToken(token).getExp()>0):
+            return False
+        
+        return True
+
+
+
+
+    
